3fe18a4bf666e6acdea06e9d52a6725775bb4558
Bwambocos <mijyumaru2525@outlook.jp>
2017-08-03 21:51:34 +0900

    Menu: 選択肢 初期化・描画追加
    ・選択肢の初期化・描画の処理を追加

--- AC_Dash/Menu.cpp
+++ AC_Dash/Menu.cpp
@@ -3,9 +3,14 @@
 #include "SceneMgr.h"
 #include "Menu.h"
 
+// define
+#define DRAW_AC_WA_SPEED 3
+
 // グローバル変数
 static Texture ac, wa;
+static Font titleFont, choiceFont;
 static int32 draw_ac_x, draw_wa_x;
+static Circle SBoardCircle, playCircle, exitCircle;
 
 // メニュー 初期化
 void Menu_Init()
@@ -15,7 +20,18 @@ void Menu_Init()
 		ac = Texture(L"data\\Menu\\ac.png");
 		wa = Texture(L"data\\Menu\\wa.png");
 		draw_ac_x = -ac.width;
-		draw_wa_x = Window::Width() + wa.width;
+		draw_wa_x = Window::Width();
+	}
+
+	// 選択肢 初期化
+	{
+		titleFont = Font(64);
+		choiceFont = Font(24);
+		SBoardCircle = playCircle = exitCircle = Circle(128);
+		SBoardCircle.x = Window::Center().x - SBoardCircle.r * 2 - 25;
+		playCircle.x = Window::Center().x;
+		exitCircle.x = Window::Center().x + exitCircle.r * 2 + 25;
+		SBoardCircle.y = playCircle.y = exitCircle.y = 25 * 2 + titleFont.height + exitCircle.r;
 	}
 }
 
@@ -24,8 +40,8 @@ void Menu_Update()
 {
 	// 背景 更新
 	{
-		draw_ac_x = (draw_ac_x >= Window::Width() ? -ac.width : draw_ac_x + 3);
-		draw_wa_x = (draw_wa_x < -wa.width ? Window::Width() + wa.width : draw_wa_x - 2);
+		draw_ac_x = (draw_ac_x >= Window::Width() ? -ac.width : draw_ac_x + DRAW_AC_WA_SPEED);
+		draw_wa_x = (draw_wa_x <= -wa.width ? Window::Width() : draw_wa_x - DRAW_AC_WA_SPEED);
 	}
 }
 
@@ -36,5 +52,22 @@ void Menu_Draw()
 	{
 		ac.draw(draw_ac_x, Window::Center().y - ac.height);
 		wa.draw(draw_wa_x, Window::Center().y);
+	}
+
+	// 選択肢 描画
+	{
+		titleFont(L"AC Dash v1.0").drawCenter(25);
+		SBoardCircle.drawShadow({ 0, 8 }, 28, 6);
+		SBoardCircle.draw(Palette::Gold);
+		if (SBoardCircle.mouseOver) { SBoardCircle.drawFrame(5, 5, Palette::Red); }
+		choiceFont(L"スコアボード\n　 を見る").drawCenter(SBoardCircle.x, SBoardCircle.y, Palette::Black);
+		playCircle.drawShadow({ 0, 8 }, 28, 6);
+		playCircle.draw(Palette::Lightskyblue);
+		if (playCircle.mouseOver) { playCircle.drawFrame(5, 5, Palette::Red); }
+		choiceFont(L"ゲーム開始！").drawCenter(playCircle.x, playCircle.y, Palette::Black);
+		exitCircle.drawShadow({ 0, 8 }, 28, 6);
+		exitCircle.draw(Palette::Orange);
+		if (exitCircle.mouseOver) { exitCircle.drawFrame(5, 5, Palette::Red); }
+		choiceFont(L"終了する").drawCenter(exitCircle.x, exitCircle.y, Palette::Black);
 	}
 }
