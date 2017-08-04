120ee58cc184ba2d4599b98f2a322ecaabfdd58f
Bwambocos <mijyumaru2525@outlook.jp>
2017-08-04 15:58:33 +0900

    Game: 背景 追加
    ・背景の処理追加

--- AC_Dash/Game.cpp
+++ AC_Dash/Game.cpp
@@ -3,20 +3,40 @@
 #include "SceneMgr.h"
 #include "Game.h"
 
+// ƒOƒ[ƒoƒ‹•Ï”
+static Texture main, ground;
+static int draw_ground_x1, draw_ground_x2, draw_speed;
+
 // ƒQ[ƒ€ ‰Šú‰»
 void Game_Init()
 {
-
+	// ”wŒi ‰Šú‰»
+	{
+		main = Texture(L"data\\Game\\main.png");
+		ground = Texture(L"data\\Game\\ground.png");
+		draw_ground_x1 = 0;
+		draw_ground_x2 = Window::Width();
+		draw_speed = 5;
+	}
 }
 
 // ƒQ[ƒ€ XV
 void Game_Update()
 {
-
+	// ”wŒi XV
+	{
+		draw_ground_x1 = (draw_ground_x1 <= -Window::Width() ? Window::Width() : draw_ground_x1 - draw_speed);
+		draw_ground_x2 = (draw_ground_x2 <= -Window::Width() ? Window::Width() : draw_ground_x2 - draw_speed);
+	}
 }
 
 // ƒQ[ƒ€ •`‰æ
 void Game_Draw()
 {
-
+	// ”wŒi •`‰æ
+	{
+		main.draw();
+		ground.draw(draw_ground_x1, 0);
+		ground.draw(draw_ground_x2, 0);
+	}
 }
