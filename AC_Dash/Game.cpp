3d017ffcb2ff63a4d7d4d186192cc7b14f3d0735
Bwambocos <mijyumaru2525@outlook.jp>
2017-08-05 15:24:40 +0900

    Game: ステータス 追加
    ・ステータスの処理を追加

--- AC_Dash/Game.cpp
+++ AC_Dash/Game.cpp
@@ -4,19 +4,36 @@
 #include "Game.h"
 
 // グローバル変数
-static Texture main, ground;
+static Texture main, ground, ac, wa;
+static Font statsFont;
 static int draw_ground_x1, draw_ground_x2, draw_speed;
+static int64 score, life;
 
 // ゲーム 初期化
 void Game_Init()
 {
 	// 背景 初期化
 	{
-		main = Texture(L"data\\Game\\main.png");
-		ground = Texture(L"data\\Game\\ground.png");
+		if (!main)
+		{
+			main = Texture(L"data\\Game\\main.png");
+			ground = Texture(L"data\\Game\\ground.png");
+		}
 		draw_ground_x1 = 0;
 		draw_ground_x2 = Window::Width();
 		draw_speed = 5;
+	}
+
+	// ステータス初期化
+	{
+		if (!ac)
+		{
+			ac = Texture(L"data\\Game\\ac.png");
+			wa = Texture(L"data\\Game\\wa.png");
+			FontManager::Register(L"data\\Game\\scoreboard.ttf");
+			statsFont = Font(32, L"Score Board");
+			score = 0; life = 5;
+		}
 	}
 }
 
@@ -28,6 +45,11 @@ void Game_Update()
 		draw_ground_x1 = (draw_ground_x1 <= -Window::Width() ? Window::Width() : draw_ground_x1 - draw_speed);
 		draw_ground_x2 = (draw_ground_x2 <= -Window::Width() ? Window::Width() : draw_ground_x2 - draw_speed);
 	}
+
+	// ステータス 更新
+	{
+		++score;
+	}
 }
 
 // ゲーム 描画
@@ -38,5 +60,11 @@ void Game_Draw()
 		main.draw();
 		ground.draw(draw_ground_x1, 0);
 		ground.draw(draw_ground_x2, 0);
+	}
+
+	// ステータス 描画
+	{
+		auto display = Format(L"SCORE:", score, L"\nLIFE :", life);
+		statsFont(display).draw(10, 10);
 	}
 }
